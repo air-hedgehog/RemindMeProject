04662675634c3d7146c1caf0a9d105f284e1eae7
lonelyfugitive <mrtalization25@gmail.com>
2016-01-22 06:03:24 +0300

    added new tabs, fab.

--- RemindMe/app/src/main/java/com/qoobico/remindme/MainActivity.java
+++ RemindMe/app/src/main/java/com/qoobico/remindme/MainActivity.java
@@ -10,7 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
-import com.qoobico.remindme.adapter.TabsPagerFragmentAdapter;
+import com.qoobico.remindme.adapter.TabsFragmentAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -47,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void initTabs() {
         viewPager = (ViewPager) findViewById(R.id.view_pager);
-        TabsPagerFragmentAdapter adapter = new TabsPagerFragmentAdapter(getSupportFragmentManager());
+        TabsFragmentAdapter adapter = new TabsFragmentAdapter(this, getSupportFragmentManager());
         viewPager.setAdapter(adapter);
 
         TabLayout tabLayout  = (TabLayout) findViewById(R.id.tab_layout);
